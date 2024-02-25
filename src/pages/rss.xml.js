1f1663dfa36cc984b091382e58965db9071dc56c
FedjaW <f.wilms89@web.de>
2024-02-25 12:31:56 +0100

    npm run format

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,11 @@
-import rss, {pagesGlobToRssItems} from "@astrojs/rss";
+import rss, { pagesGlobToRssItems } from '@astrojs/rss'
 
 export async function GET(context) {
   return rss({
-    title: "Kochbuch | Rezepte",
-    description: "Die besten Rezepte der Welt",
+    title: 'Kochbuch | Rezepte',
+    description: 'Die besten Rezepte der Welt',
     site: context.site,
-    items: await pagesGlobToRssItems(import.meta.glob("./**/*.mdx")),
+    items: await pagesGlobToRssItems(import.meta.glob('./**/*.mdx')),
     customData: `<language>de-de</language>`,
-  });
+  })
 }
